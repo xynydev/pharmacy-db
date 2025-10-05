6a95cfc3625a0e2a921bdb5e8d0d1e660115aaa5
xyny <git@xyny.anonaddy.me>
2025-10-05 14:17:01 +0300

    feat: first version of schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,23 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, numeric, text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
+export const pharmacy = sqliteTable('pharmacy', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	lat: text('lat').notNull(),
+	lon: text('lon').notNull(),
+	country: text({ enum: ['Finland'] }).notNull(),
+	name: text('name').notNull(),
+	brand: text('brand'),
+	description: text('description'),
+	address: text('address'),
+	email: text('email'),
+	phone: text('phone'),
+	opening_hours: text('opening_hours'),
+	url: text('url')
+});
+
+export const report = sqliteTable('report', {
+	id: integer('id').primaryKey(),
+	pharmacyId: integer('pharmacy_id').references(() => pharmacy.id),
+	report: text({ enum: ['++', '+', '-'] }).notNull(),
+	extraInfo: text('extra_info')
 });
