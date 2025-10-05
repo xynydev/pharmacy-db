b9b0f4fe56883ac465f03d01d91adb3efb3057cd
xyny <git@xyny.anonaddy.me>
2025-10-05 14:16:34 +0300

    fix: use platform object to get DB binding

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/d1';
 import * as schema from './schema';
 
-import { DB } from '$env/static/private';
-
-export const db = drizzle(DB, { schema });
+export function getDb(platform: Readonly<App.Platform> | undefined) {
+	return drizzle(platform?.env.DB, { schema });
+}
